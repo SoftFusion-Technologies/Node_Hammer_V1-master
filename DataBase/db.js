1fb5d4f3e687aa0bac42691af946986d2ac87c2a
Benjamin <goosta19802@gmail.com>
2024-08-06 17:41:04 -0300

    Configuración para producción y actualización de .gitignore

--- DataBase/db.js
+++ DataBase/db.js
@@ -7,17 +7,21 @@ import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } from './config.js';
 // para realizar la conexion de forma muy sensilla es poner los parametros dentro de la instancia
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
-  port: DB_PORT, // Agrega el puerto a la configuración
+  port: DB_PORT,
   dialect: 'mysql',
   define: { freezeTableName: true },
   pool: {
-    max: 5,
-    min: 0,
+    max: 15, 
+    min: 5,
     acquire: 30000,
     idle: 10000
   },
   dialectOptions: {
-    connectTimeout: 60000 // Tiempo de espera en milisegundos (60 segundos)
+    connectTimeout: 60000,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
 });
 
--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,21 +1,27 @@
 module.exports = {
   apps: [
     {
-      name: 'Node_Hammer_V1-master', // Cambia aquí al nombre de tu aplicación
-      script: 'app.js', // Cambia esto si tu archivo principal es diferente
-      instances: 'max', // Ejecuta tantas instancias como CPUs tengas
-      exec_mode: 'cluster', // Modo cluster para aprovechar múltiples CPUs
+      name: 'Node_Hammer_V1-master',
+      script: 'app.js',
+      instances: 'max',
+      exec_mode: 'cluster',
       env: {
-        NODE_ENV: 'development' // Variables de entorno para el entorno de desarrollo
+        NODE_ENV: 'development',
+        PORT: 3000,
+        DB_HOST: 'localhost',
+        DB_USER: 'root',
+        DB_PASSWORD: 123456,
+        DB_NAME: 'development_db',
+        DB_PORT: 3306
       },
       env_production: {
-        NODE_ENV: 'production', // Variables de entorno para el entorno de producción
-        PORT: 3000, // Puerto en el que tu aplicación escuchará
-        DB_HOST: '149.50.141.175', // IP del servidor de base de datos
+        NODE_ENV: 'production',
+        PORT: 3000,
+        DB_HOST: '149.50.141.175',
         DB_USER: 'root',
         DB_PASSWORD: '8aKPXCd25GBR',
         DB_NAME: 'c1841398_hammer',
-        DB_PORT: 5417 // Puerto de la base de datos
+        DB_PORT: 5417
       }
     }
   ]
