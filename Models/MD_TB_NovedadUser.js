4f3db63350144b2a60023aa77ebd877329ecb720
Benjamin <goosta19802@gmail.com>
2025-04-05 14:18:40 -0300

    Solucionado

--- Models/MD_TB_NovedadUser.js
+++ Models/MD_TB_NovedadUser.js
@@ -3,28 +3,38 @@ import { DataTypes } from 'sequelize';
 import NovedadesModel from './MD_TB_Novedades.js';
 import UsersModel from './MD_TB_Users.js';
 
-const NovedadUserModel = db.define('novedad_user', {
-  novedad_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false
+const NovedadUserModel = db.define(
+  'novedad_user',
+  {
+    novedad_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    leido: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
   },
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  leido: {
-    type: DataTypes.INTEGER,
-    allowNull: false
+  {
+    timestamps: false
   }
-}, {
-  timestamps: false
-});
+);
 
 // Definir asociaciones
-NovedadesModel.hasMany(NovedadUserModel, { foreignKey: 'novedad_id', as: 'novedadUsers' });
+NovedadesModel.hasMany(NovedadUserModel, {
+  foreignKey: 'novedad_id',
+  as: 'novedadUsers'
+});
 NovedadUserModel.belongsTo(NovedadesModel, { foreignKey: 'novedad_id' });
 
 NovedadUserModel.belongsTo(UsersModel, { foreignKey: 'user_id', as: 'user' });
-UsersModel.hasMany(NovedadUserModel, { foreignKey: 'user_id', as: 'novedadUsers' });
+UsersModel.hasMany(NovedadUserModel, {
+  foreignKey: 'user_id',
+  as: 'novedadUsers'
+});
 
 export default NovedadUserModel;
