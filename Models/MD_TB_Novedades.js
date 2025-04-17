b38a9a2808e2f1fa1d9efb4be09bb689e03e0263
Benjamin <goosta19802@gmail.com>
2025-04-16 21:21:03 -0300

    Se agrega nuevo campo user Name a novedades

--- Models/MD_TB_Novedades.js
+++ Models/MD_TB_Novedades.js
@@ -11,22 +11,26 @@
 */
 // Importa la configuración de la base de datos y los tipos de datos necesarios
 // Importa la configuración de la base de datos y los tipos de datos necesarios
-import dotenv from "dotenv";
-import db from "../DataBase/db.js";
-import { DataTypes } from "sequelize";
+import dotenv from 'dotenv';
+import db from '../DataBase/db.js';
+import { DataTypes } from 'sequelize';
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
 }
 
 const NovedadesModel = db.define(
-  "novedades",
+  'novedades',
   {
     sede: { type: DataTypes.STRING, allowNull: false },
     titulo: { type: DataTypes.STRING },
     mensaje: { type: DataTypes.TEXT, allowNull: false },
     vencimiento: { type: DataTypes.DATE },
     estado: { type: DataTypes.TINYINT, allowNull: false, defaultValue: 1 },
+    userName: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    }
   },
   { timestamps: false }
 );
